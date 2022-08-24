96e341ee347108c3f69d12f79b0b13248421bdbb
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-08-24 11:03:14 -0400

    Tried to replace literals with objects

--- app.js
+++ app.js
@@ -1,472 +1,97 @@
-Seattle = { 
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  avgCookies: function () { //average cookies per customer per hour
-    return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-  },
-  addHtml: function () { //adds html to the table
-    let html = `
-    <h1>Seattle</h1>
-    <table>
-      <tr>
-        <th>Time</th>
-        <th>Cookies</th>
-        </tr>
-        <tr>
-        <td>6:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>8:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>9:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>10:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>11:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>12:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>1:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>2:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>3:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>4:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>5:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>6:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>        
-        </table>
-        <h2>Total: ${this.total()} cookies</h2>
-        `;
-    document.getElementById("seattle").innerHTML = html;
-  },  
-  total: function () { //total cookies sold
-    let total = 0;
-    for (let i = 0; i < 14; i++) {
-      total += this.avgCookies();
-    }
-    return total;
-  },
-};
+let stores = [];
 
-Tokyo = {
-  min: 3,
-  max: 24,
-  avg: 1.2,
-  avgCookies: function () { //average cookies per customer per hour
-    return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-  },
-  total: function () { //total cookies sold
-    let total = 0;
-    for (let i = 0; i < 14; i++) {
-      total += this.avgCookies();
-    }
-    return total;
-  },
-  addHtml: function () { //adds html to the table
-    let html = `
-    <h1>Tokyo</h1>
-    <table>
-      <tr>
-        <th>Time</th>
-        <th>Cookies</th>
-        </tr>
-        <tr>
-        <td>6:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>8:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>9:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>10:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>11:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>12:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>1:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>2:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>3:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>4:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>5:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>6:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>        
-        </table>
-        <h2>Total: ${this.total()} cookies</h2>
-        `;
-    document.getElementById("tokyo").innerHTML = html;
-  },
-};
-
-Dubai = {
-  min: 11,
-  max: 38,
-  avg: 3.7,
-  avgCookies: function () {  //average cookies per customer per hour
-    return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-  },
-  total: function () { //total cookies sold
-    let total = 0;
-    for (let i = 0; i < 14; i++) {
-      total += this.avgCookies();
-    }
-    return total;
-  },
-  addHtml: function () {    //adds html to the table
-    let html = `
-    <h1>Dubai</h1>
-    <table>
-      <tr>
-        <th>Time</th>
-        <th>Cookies</th>
-        </tr>
-        <tr>
-        <td>6:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>8:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>9:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>10:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>11:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>12:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>1:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>2:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>3:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>4:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>5:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>6:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>        
-        </table>
-        <h2>Total: ${this.total()} cookies</h2>
-        `;
-    document.getElementById("dubai").innerHTML = html;
-  },
-};
-
-Paris = {
-  min: 20,
-  max: 38,
-  avg: 2.3,
-  avgCookies: function () { //average cookies per customer per hour
-    return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-  },
-  total: function () {  //total cookies sold
-    let total = 0;
-    for (let i = 0; i < 14; i++) {
-      total += this.avgCookies();
-    }
-    return total;
-  },
-  addHtml: function () {    //adds html to the table
-    let html = `
-    <h1>Paris</h1>
-    <table>
-      <tr>
-        <th>Time</th>
-        <th>Cookies</th>
-        </tr>
-        <tr>
-        <td>6:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>8:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>9:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>10:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>11:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>12:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>1:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>2:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>3:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>4:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>5:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>6:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>        
-        </table>
-        <h2>Total: ${this.total()} cookies</h2>
-        `;
-    document.getElementById("paris").innerHTML = html;
-  },
-};
-
-Lima = {
-  min: 2,
-  max: 16,
-  avg: 4.6,
-  avgCookies: function () { //average cookies per customer per hour
-    return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-  },
-  total: function () { //total cookies sold
-    let total = 0;
-    for (let i = 0; i < 14; i++) {
-      total += this.avgCookies();
-    }
-    return total;
-  },
-  addHtml: function () { //adds html to the table
-    let html = `
-    <h1>Lima</h1>
-    <table>
-      <tr>
-        <th>Time</th>
-        <th>Cookies</th>
-        </tr>
-        <tr>
-        <td>6:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>8:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>9:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>10:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>11:00am</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>12:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>1:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>2:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>3:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>4:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>5:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>6:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>
-        <tr>
-        <td>7:00pm</td>
-        <td>${this.avgCookies()} cookies</td>
-        </tr>        
-        </table>
-        <h2>Total: ${this.total()} cookies</h2>
-        `;
-    document.getElementById("lima").innerHTML = html;
-  },
-};
-
-
-function addTable(){
-    //Variables
-    let h1 = document.createElement("h1");
-    let table = document.createElement("table");
-    let total = document.createElement("h2");
-    let header = document.createElement("th");
-    let header1 = document.createElement("th");
-    let header2 = document.createElement("th");
-
-    //Content
-    header1.innerHTML = "Time " ; 
-    header2.innerHTML = "Cookies " ;
-    header.appendChild(header1); //append header1 to header
-    header.appendChild(header2); //append header2 to header
-    table.appendChild(header); //append header to table
-    h1.innerHTML = "Seattle"; //set h1 content
-    total.innerHTML = "Total: " + Seattle.total() + " cookies"; //total cookies
-
-    for(let i = 6; i <= 12; i++){ //loop for 6am to 12pm
-        let row = document.createElement("tr"); //create row
-        let time = document.createElement("td"); //create td for time
-        time.innerHTML = i + "am: " + " " + Seattle.avgCookies() + " cookies"; //set time content
-        row.appendChild(time);
-        table.appendChild(row);
-    }
-    for(let i = 1; i <= 7; i++){ //loop for 1pm to 7pm
-        let row = document.createElement("tr");
-        let time = document.createElement("td");
-        time.innerHTML = i + "pm: " + " " + Seattle.avgCookies() + " cookies";
-        row.appendChild(time);
-        table.appendChild(row);
-    }
-
-    //Append
-    document.getElementById("seattle").appendChild(h1); //append h1 to seattle
-    document.getElementById("seattle").appendChild(table); //append table to seattle
-    document.getElementById("seattle").appendChild(total); //append total to seattle
+function Store(
+  name,
+  hourOpen,
+  hourClosed,
+  minCust,
+  maxCust,
+  avgSoldCookies,
+  hourlyTotals
+) {
+  this.name = name;
+  this.hourOpen = hourOpen;
+  this.hourClosed = hourClosed;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgSoldCookies = avgSoldCookies;
+  this.hourlyTotals = hourlyTotals;
 }
 
-addTable();
-Tokyo.addHtml();
-Dubai.addHtml();
-Paris.addHtml();
-Lima.addHtml();
+Store.prototype.custPerHour = function () {
+  return Math.floor(
+    this.minCust + Math.random() * (this.maxCust - this.minCust + 1)
+  );
+};
 
+Store.prototype.calcCustPerHour = function () {
+  let hourTotals = [];
+  let hourTotalsIndex = 0;
+  for (let i = this.hourOpen; i < this.hourClosed; i++) {
+    hourTotals[hourTotalsIndex] = Math.round(
+      this.custPerHour() * this.avgSoldCookies
+    );
+    hourTotalsIndex++;
+  }
+  this.hourlyTotals = hourTotals;
+};
 
+function addStore(data) {
+  stores.push(
+    new Store(
+      data.name,
+      data.hourOpen,
+      data.hourClosed,
+      data.minCust,
+      data.maxCust,
+      data.avgSoldCookies,
+      data.hourlyTotals
+    )
+  );
+}
+
+addStore({
+  name: "seattle",
+  hourOpen: 6,
+  hourClosed: 20,
+  minCust: 23,
+  maxCust: 65,
+  avgSoldCookies: 6.3,
+  hourlyTotals: [],
+});
+addStore({
+  name: "tokyo",
+  hourOpen: 6,
+  hourClosed: 20,
+  minCust: 3,
+  maxCust: 24,
+  avgSoldCookies: 1.2,
+  hourlyTotals: [],
+});
+addStore({
+  name: "dubai",
+  hourOpen: 6,
+  hourClosed: 20,
+  minCust: 11,
+  maxCust: 38,
+  avgSoldCookies: 3.7,
+  hourlyTotals: [],
+});
+addStore({
+  name: "paris",
+  hourOpen: 6,
+  hourClosed: 20,
+  minCust: 20,
+  maxCust: 38,
+  avgSoldCookies: 2.3,
+  hourlyTotals: [],
+});
+addStore({
+  name: "lima",
+  hourOpen: 6,
+  hourClosed: 20,
+  minCust: 2,
+  maxCust: 16,
+  avgSoldCookies: 4.6,
+  hourlyTotals: [],
+});
